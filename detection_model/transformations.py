b848280102677b6ebc3933efbdd71dea2d5d2827
pn3270 <pn3270@rit.edu>
2025-10-29 22:50:29 -0400

    Adding terraform scripts

--- detection_model/transformations.py
+++ detection_model/transformations.py
@@ -3,10 +3,7 @@ import cv2 as cv
 import numpy as np
 
 persons = ["Shreya", "Praneet"]
-
 DIR = r"/Users/praneetnaik/Desktop/Attendance-Automation-System/detection_model/train"
-
-p = []
 
 haar_cascade = cv.CascadeClassifier('detection_model/classifier/haar_face.xml')
 
@@ -20,7 +17,6 @@ def train_image():
 
         for img in os.listdir(path):
             img_path = os.path.join(path,img)
-
             img_array = cv.imread(img_path)
             if img_array is None:
                 continue
@@ -29,20 +25,21 @@ def train_image():
 
             faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
 
-            for (x,y,w,h) in faces_rect:
+            for (x, y, w, h) in faces_rect:
                 faces_roi = gray[y:y+h, x:x+w]
-                face_roi = cv.resize(faces_roi, (200, 200))
                 features.append(faces_roi)
                 labels.append(label)
 
 train_image()
 print('Training done ---------------')
 
+# Convert labels to int32 (features can stay as object)
 features = np.array(features, dtype='object')
 labels = np.array(labels)
 
+
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
-face_recognizer.train(features,labels)
+face_recognizer.train(features, labels)
 
 face_recognizer.save('face_trained.yml')
 np.save('features.npy', features)
