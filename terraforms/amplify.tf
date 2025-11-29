19d9c1559fe8f22f320c6f331cd1b4bb2c8054f0
pn3270 <pn3270@rit.edu>
2025-11-29 12:15:52 -0500

    Api issue resolution

--- terraforms/amplify.tf
+++ terraforms/amplify.tf
@@ -20,7 +20,7 @@ resource "aws_amplify_app" "my_app" {
                 - npm install
           build:
             commands:
-                - echo "REACT_APP_API_ENDPOINT= ${aws_api_gateway_stage.deployment.invoke_url}" >> .env.production
+                - echo "REACT_APP_API_ENDPOINT=https://${aws_api_gateway_rest_api.ams_apis_tf.id}.execute-api.us-east-1.amazonaws.com/dev" >> .env.production
                 - echo "REACT_APP_aws_cognito_identity_pool_id=${aws_cognito_identity_pool.my_identity_pool.id}" >> .env.production
                 - echo "REACT_APP_aws_user_pools_id=${aws_cognito_user_pool.my_user_pool.id}" >> .env.production
                 - echo "REACT_APP_aws_user_pools_web_client_id=${aws_cognito_user_pool_client.my_user_pool_client.id}" >> .env.production
